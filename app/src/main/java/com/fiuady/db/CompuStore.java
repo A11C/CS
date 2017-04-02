0e4b1e98b0f7730cd3d0d4f9aa2dce249aac9b84
Wilo9517 <cadw951117@gmail.com>
2017-04-02 17:04:48 -0500

    Avance 2

---
+++ app/src/main/java/com/fiuady/db/CompuStore.java
@@ -0,0 +1,192 @@
+package com.fiuady.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fiuady.db.CompuStoreDbSchema.*;
+
+class CategoryCursor extends CursorWrapper {
+    public CategoryCursor(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Category getCategory(){
+        Cursor cursor = getWrappedCursor();
+        return new Category(cursor.getInt(cursor.getColumnIndex(CompuStoreDbSchema.CategoriesTable.Columns.ID)),cursor.getString(cursor.getColumnIndex(CompuStoreDbSchema.CategoriesTable.Columns.DESCRIPTION)));
+    }
+}
+
+class ProductCursor extends CursorWrapper {
+    public ProductCursor(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Product getProduct(){
+        Cursor cursor = getWrappedCursor();
+        return new Product(cursor.getInt(cursor.getColumnIndex(CompuStoreDbSchema.ProductsTable.Columns.ID)),cursor.getInt(cursor.getColumnIndex(ProductsTable.Columns.CATEGORY_ID)),
+                cursor.getString(cursor.getColumnIndex(CompuStoreDbSchema.ProductsTable.Columns.DESCRIPTION)), cursor.getInt(cursor.getColumnIndex(ProductsTable.Columns.PRICE)),
+                cursor.getInt(cursor.getColumnIndex(ProductsTable.Columns.QUANTITY)));
+    }
+}
+
+class ClientCursor extends CursorWrapper {
+    public ClientCursor(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Client getClient(){
+        Cursor cursor = getWrappedCursor();
+        return new Client(cursor.getInt(cursor.getColumnIndex(CompuStoreDbSchema.CustomersTable.Columns.ID)),cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.FIRST_NAME)),
+                cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.LAST_NAME)),cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.ADDRESS)),
+                cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.E_MAIL)),cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.PHONE1)),
+                cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.PHONE2)),cursor.getString(cursor.getColumnIndex(CustomersTable.Columns.PHONE3)));
+    }
+}
+
+public final class CompuStore {
+    private CompuStoreHelper compuStoreHelper;
+    private SQLiteDatabase db;
+
+    private List<Category> categories;
+
+    public CompuStore(Context context) {
+        compuStoreHelper = new CompuStoreHelper(context);
+        db = compuStoreHelper.getWritableDatabase();
+        compuStoreHelper.backupDatabasefile(context);
+    }
+
+    // ------------------------------------------------------ CATEGORIES --------------------------------------------------------
+
+    public List<Category> getAllCategories() {
+        ArrayList<Category> list = new ArrayList<>();
+
+        CategoryCursor cursor = new CategoryCursor(db.rawQuery("SELECT * FROM product_categories ORDER BY id", null));
+        while(cursor.moveToNext()){
+            list.add(cursor.getCategory());
+        }
+        cursor.close();
+
+        return list;
+    }
+
+    public boolean updateCategory(String des, int id) {
+        boolean b = true;
+        List<Category> a = getAllCategories();
+
+        if (des.isEmpty()) {
+            b = false;
+        }
+
+        for(Category category : a) {
+            if (category.getDescription().toUpperCase().equals(des.toUpperCase())) {
+                b = false;
+            }
+        }
+
+        if (b) {
+            ContentValues values = new ContentValues();
+            values.put(CategoriesTable.Columns.DESCRIPTION, des);
+
+            db.update(CategoriesTable.NAME,
+                    values,
+                    CategoriesTable.Columns.ID+ "= ?",
+                    new String[] {Integer.toString(id)});
+        }
+
+        return b;
+    }
+
+    public boolean insertCategory(String text) {
+        boolean b = true;
+        List<Category> a = getAllCategories();
+        ContentValues values = new ContentValues();
+
+        if (text.isEmpty()) {
+            b = false;
+        }
+
+        for(Category category : a) {
+            if (category.getDescription().toUpperCase().equals(text.toUpperCase())) {
+
+                b = false;
+            }
+        }
+
+        if (b) {
+            Category c = a.get(a.size()-1);
+
+            values.put(CategoriesTable.Columns.DESCRIPTION, text);
+
+            db.insert(CategoriesTable.NAME, null, values);
+        }
+
+        return b;
+    }
+
+    public boolean deleteCategory(int id, boolean dlt) {
+        boolean c = false;
+        boolean d = true;
+        boolean e = true;
+        List<Category> a = getAllCategories();
+        List<Product> b = getAllProducts();
+
+        for(Category category : a) {
+            if (e) {
+                if (category.getId() == id) {  // Condicion si la categoria ya exite en categorias
+                    e = false;
+                    if (d) {
+                        for(Product product : b) {
+                            if (product.getCategory_id() == id) {  // Condicion si algun producto tiene asignado la categoria
+                                c = true;
+                                d = false;
+                            }
+                            else {
+                                if (dlt){  // Quiero elimanrlo?
+                                    db.delete(CategoriesTable.NAME, CategoriesTable.Columns.ID + "= ?",
+                                            new String[] {Integer.toString(id)});
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return  c;
+    }
+
+    // -------------------------------------------------------- PRODUCTS --------------------------------------------------------
+
+    public List<Product> getAllProducts() {
+        ArrayList<Product> list = new ArrayList<>();
+
+        ProductCursor cursor = new ProductCursor(db.rawQuery("SELECT * FROM products ORDER BY id", null));
+        while(cursor.moveToNext()) {
+            list.add(cursor.getProduct());
+        }
+        cursor.close();
+
+        return list;
+    }
+
+    // -------------------------------------------------------- CLIENTS --------------------------------------------------------
+
+    public List<Client> getAllClients() {
+        ArrayList<Client> list = new ArrayList<>();
+
+        ClientCursor cursor = new ClientCursor(db.rawQuery("SELECT * FROM customers ORDER BY id", null));
+        while(cursor.moveToNext()){
+            list.add(cursor.getClient());
+        }
+        cursor.close();
+
+        return list;
+    }
+}
