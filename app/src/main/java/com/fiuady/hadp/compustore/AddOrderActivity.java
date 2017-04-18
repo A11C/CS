0dd1d24779899316a4c23e0ce12c98ba5be6e133
jessmmq <jessmonjiote@gmail.com>
2017-04-18 15:51:12 -0500

    Orders

--- app/src/main/java/com/fiuady/hadp/compustore/AddOrderActivity.java
+++ app/src/main/java/com/fiuady/hadp/compustore/AddOrderActivity.java
@@ -10,10 +10,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.fiuady.db.Assembly;
 import com.fiuady.db.AssemblyProduct;
+import com.fiuady.db.Client;
 import com.fiuady.db.CompuStore;
 import com.fiuady.db.Order;
 import com.fiuady.db.Product;
@@ -23,6 +26,7 @@ import java.util.List;
 /**
  * Created by jessm on 18/04/2017.
  */
+
 
 public class AddOrderActivity extends AppCompatActivity {
     private class OrderHolder extends RecyclerView.ViewHolder {
@@ -71,9 +75,10 @@ public class AddOrderActivity extends AppCompatActivity {
     }
 
     private CompuStore compustore;
+
     private RecyclerView recyclerview;
     private OrderAdapter adapter;
-
+    private Spinner spinner_clients;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.new_order);
@@ -83,8 +88,29 @@ public class AddOrderActivity extends AppCompatActivity {
         recyclerview = (RecyclerView) findViewById(R.id.clients_rv);
         recyclerview.setLayoutManager(new LinearLayoutManager(this));
         adapter = new OrderAdapter(compustore.getAllAssemblyProducts());
-
+        spinner_clients = (Spinner) findViewById(R.id.spinner_clients);
         recyclerview.setAdapter(adapter);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item);
+        spinner_clients.setAdapter(arrayAdapter);
+
+        final List<Client> clients = compustore.getAllClients();
+        for (Client client : clients) {
+            arrayAdapter.add(client.getFirst_name()+" "+client.getLast_name());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.agregar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent(AddOrderActivity.this, OrdersActivity.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
 
--- app/src/main/java/com/fiuady/hadp/compustore/OrdersActivity.java
+++ app/src/main/java/com/fiuady/hadp/compustore/OrdersActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -18,21 +20,83 @@ import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.fiuady.MultiSelectionSpinner;
+import com.fiuady.StateC;
+import com.fiuady.db.AssemblyProduct;
+import com.fiuady.db.Category;
+import com.fiuady.db.Client;
+import com.fiuady.db.CompuStore;
+import com.fiuady.db.Order;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class OrdersActivity extends AppCompatActivity {
+
 
     private static boolean datei, datef;
     private Spinner spinneror, spinnercl;
     private CheckBox chkdi, chkdf;
     private TextView datefin, dateini;
-    private RecyclerView recyclerview;
     private static String string;
+    private CompuStore compustore;
+    private RecyclerView recyclerview;
+    private OrderAdapter adapter;
 
-    public static class DatePickerFragment extends DialogFragment
+    private class OrderHolder extends RecyclerView.ViewHolder {
+
+        private TextView idOrtext, Statustext, CustomerIdtext,dateOrtext;
+
+        public OrderHolder(View itemView) {
+            super(itemView);
+            idOrtext = (TextView) itemView.findViewById(R.id.id_Or_text);
+            Statustext = (TextView) itemView.findViewById(R.id.estado_text);
+            CustomerIdtext = (TextView) itemView.findViewById(R.id.idclient_text);
+            dateOrtext = (TextView) itemView.findViewById(R.id.fecha_text);
+
+        }
+
+        private void bindOrder(final Order order) {
+
+            idOrtext.setText(Integer.toString(order.getId()));
+            Statustext.setText(Integer.toString(order.getStatus_id()));
+            CustomerIdtext.setText(Integer.toString(order.getCustomer_id()));
+            dateOrtext.setText(order.getDate());
+
+        }
+    }
+    private class OrderAdapter extends RecyclerView.Adapter<OrdersActivity.OrderHolder> {
+
+        private List<Order> orders;
+
+        public OrderAdapter(List<Order> orders) {
+            this.orders = orders;
+        }
+
+        @Override
+        public OrderHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.list_orders, parent, false);
+            return new OrdersActivity.OrderHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(OrderHolder holder, int position) {
+            holder.bindOrder(orders.get(position));
+        }
+
+        @Override
+        public int getItemCount()
+            {
+                return orders.size();
+            }
+
+    }
+
+        public static class DatePickerFragment extends DialogFragment
             implements DatePickerDialog.OnDateSetListener {
 
         @Override
@@ -58,26 +122,39 @@ public class OrdersActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
-        spinnercl = (Spinner) findViewById(R.id.spinnerC);
-        spinneror = (Spinner) findViewById(R.id.spinnerOr);
+      //  spinnercl = (Spinner) findViewById(R.id.spinnerC);
+      //  spinneror = (Spinner) findViewById(R.id.spinnerOr);
         dateini = (TextView) findViewById(R.id.dateini);
         chkdf = (CheckBox) findViewById(R.id.orderFf_checkbox);
         chkdi = (CheckBox) findViewById(R.id.orderFi_checkbox);
         datefin = (TextView) findViewById(R.id.datefin);
 
 
+        final String[] select_qualification = {
+                "Filtrar por:", "Pendiente", "Cancelado", "Cofirmado", "En tránsito","Finalizado"};
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerOr);
 
+        ArrayList<StateC> listC = new ArrayList<>();
 
+        for (int i = 0; i < select_qualification.length; i++) {
+            StateC stateC = new StateC();
+            stateC.setTitle(select_qualification[i]);
+            stateC.setSelected(false);
+            listC.add(stateC);
+        }
+
+        MultiSelectionSpinner multiSelectionSpinner = new MultiSelectionSpinner(OrdersActivity.this, 0,
+                listC);
+        spinner.setAdapter(multiSelectionSpinner);
 
 
      /*  setContentView(R.layout.checkbox_orders);
         spinneror = (Spinner) findViewById(R.id.spinnerOr);
-*/
+
     /*    ArrayAdapter<String> adapter = ArrayAdapter.createFromResource(this,R.layout.checkbox_orders);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
        // spinneror.setAdapter(adapter);
@@ -96,7 +173,22 @@ public class OrdersActivity extends AppCompatActivity {
 //       arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        spinneror.setAdapter(arrayAdapter);
 //        spinneror.setAdapter(ArrayAdapter.createFromResource(R.layout.checkbox_clients));
+        spinnercl = (Spinner) findViewById(R.id.spinnerC);
+        compustore = new CompuStore(this);
 
+        recyclerview = (RecyclerView) findViewById(R.id.orders_rv);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new OrderAdapter(compustore.getAllOrders());
+        recyclerview.setAdapter(adapter);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item);
+        spinnercl.setAdapter(arrayAdapter);
+
+        arrayAdapter.add("Todos");
+        final List<Client> clients = compustore.getAllClients();
+        for (Client client : clients) {
+            arrayAdapter.add(client.getFirst_name()+" "+client.getLast_name());
+        }
 
 
         chkdf.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
