cd59db9489564fbc0b023d9a2f8bc8e9c8458a67
jessmmq <jessmonjiote@gmail.com>
2017-04-20 13:35:05 -0500

    Clients

---
+++ app/src/main/java/com/fiuady/hadp/compustore/ModClientActivity.java
@@ -0,0 +1,17 @@
+package com.fiuady.hadp.compustore;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jessm on 20/04/2017.
+ */
+
+public class ModClientActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_client);
+    }
+}
